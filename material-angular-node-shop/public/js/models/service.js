4325ca9ec05faac442081d7feaade22e01b5a967
Mayank-Ahuja <mayankahuja8894@gmail.com>
2017-05-12 17:58:36 +0530

    menu item update

--- material-angular-node-shop/public/js/models/service.js
+++ material-angular-node-shop/public/js/models/service.js
@@ -16,6 +16,22 @@
 //   return object;
 //});
 
+/*
+app.factory("shopfactory",function($http,$q){
+	var object ={};
+	object.callServer=function(){
+		var defered = $q.defer();
+		$http.get(https://raw.githubusercontent.com/amitsrivastava4all/MEANBATCH-12to2WE/master/mobile.json).then(function(data){
+			defered.resolve(data);
+		},function(error){
+			defered.reject(error);
+		})
+		return defered.promise;
+	}
+	return object;
+})
+*/
+
 app.service("menuService", function ($http, $log, $q) {
     this.object = {
         getMenu: function () {
@@ -39,19 +55,25 @@ app.service("menuService", function ($http, $log, $q) {
     }
 });
 
+app.service("quickLinksService", function ($http, $log, $q) {
+    this.object = {
+        getMenu: function () {
+            var defered = $q.defer();
+            $http({
+                    method: 'GET',
+                    url: 'https://raw.githubusercontent.com/Mayank-Ahuja/mean-practice/master/quickLinks.json'
+                })
+                .then(function (response) {
+                    $log.info(response);
+                    defered.resolve(response);
+                    var menuData = response.data;
+                    console.log("menuData: ", menuData);
 
-/*
-app.factory("shopfactory",function($http,$q){
-	var object ={};
-	object.callServer=function(){
-		var defered = $q.defer();
-		$http.get(https://raw.githubusercontent.com/amitsrivastava4all/MEANBATCH-12to2WE/master/mobile.json).then(function(data){
-			defered.resolve(data);
-		},function(error){
-			defered.reject(error);
-		})
-		return defered.promise;
-	}
-	return object;
-})
-*/
+                }, function (error) {
+                    defered.reject(error);
+                    console.log("error: ", error);
+                })
+            return defered.promise;
+        }
+    }
+});
