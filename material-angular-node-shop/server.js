ee05787eba80344456f1dc0643903dd71aeccb91
Mayank Ahuja <mayankahuja8894@gmail.com>
2017-05-15 00:08:42 +0530

    node server

---
+++ material-angular-node-shop/server.js
@@ -0,0 +1,44 @@
+var express = require("express");
+/*var session = require('express-session');*/
+var app = express();
+app.use(express.static('public'));
+
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+app.all('/*', function (req, res) {
+    console.log('All');
+    res
+        .status(200)
+        .set({
+            'content-type': 'text/html; charset=utf-8'
+        })
+        .sendfile('public/index.html');
+});
+app.post('/register', function (request, response) {
+    var dbOperation = require("./crudOperation");
+    var data = request.body;
+    console.log("Data is... ", data);
+    dbOperation.addUser(data, response);
+});
+app.post('/check', function (request, response) {
+    var dbOperation = require("./crudOperation");
+    var data = request.body;
+    console.log("Data is... ", data);
+    dbOperation.findUser(data, response);
+});
+
+app.post('/login', function (request, response) {
+    var dbOperation = require("./crudOperation");
+    var data = request.body;
+    console.log("Data is... ", data);
+    dbOperation.findUser(data, response);
+});
+
+app.listen(1234, function () {
+    console.log("server start");
+});
