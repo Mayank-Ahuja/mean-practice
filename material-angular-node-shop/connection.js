da013e6701a89bf8db71d23352c53431e6fdb7f4
Mayank Ahuja <mayankahuja8894@gmail.com>
2017-05-15 23:01:44 +0530

    deleted

--- material-angular-node-shop/connection.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require("mongoose");
-var dbConfig = require("./dbConfig");
-
-mongoose.connect(dbConfig.dbUrl);
-module.exports = mongoose;
--- material-angular-node-shop/dbConfig.js
+++
@@ -1,5 +0,0 @@
-var config = {
-    dbUrl: "mongodb://localhost:27017/usersDb"
-}
-
-module.exports = config;
